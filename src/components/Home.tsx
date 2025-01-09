bbb695080986fd289be5447b99135605326365d9
onurmurioglu <onurmurioglu@gmail.com>
2025-01-09 17:15:54 +0300

    delete title

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -74,7 +74,6 @@ const Home: React.FC = () => {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, delay: 0.5 }}
         >
-          <h2>Education & Background</h2>
           <div className="bio-content">
             <h3>Education</h3>
             <p>Computer Engineering, Iskenderun Technical University</p>
